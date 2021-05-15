9229d04cbe540e01bcf8aec5803bb572cb7ee20a
An39nA0219 <an39na10282004@yahoo.co.jp>
2021-05-16 00:36:15 +0900

    新規todoのadd処理

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [IncompleteTodos, setIncompleteTodos] = useState(["wash my hands"]);
+  const onChangeInput = (e) => {
+    setNewTodo(e.target.value);
+  };
+  const onClickInputAdd = () => {
+    const newIncompleteTodos = [...IncompleteTodos, newTodo];
+    setIncompleteTodos(newIncompleteTodos);
+    setNewTodo("");
+  };
+
   return (
     <>
       <h1>ToDo App</h1>
       <div className="inputArea">
-        <input placeholder="Input ToDo"></input>
-        <button>Add</button>
+        <input
+          placeholder="Input ToDo"
+          value={newTodo}
+          onChange={onChangeInput}
+        ></input>
+        <button onClick={onClickInputAdd}>Add</button>
       </div>
       <div className="incompleteArea">
         <h2>Incomplete Lists</h2>
         <ul>
-          <li>
-            wash my hands
-            <button>Done</button>
-            <button>Delete</button>
-          </li>
+          {IncompleteTodos.map((todo) => {
+            return (
+              <li>
+                {todo}
+                <button>Done</button>
+                <button>Delete</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="completeArea">
